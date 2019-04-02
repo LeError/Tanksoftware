1c847221d2d423c6bbea48826a26455b80213d2d
Robin Herder <45996210+leerror@users.noreply.github.com>
2019-04-02 08:41:05 +0200

    Added Getter

--- src/data/ReadFile.java
+++ src/data/ReadFile.java
@@ -8,13 +8,19 @@ public class ReadFile {
 
 	private final File FILE;
 	private final ArrayList<String> LINES = new ArrayList<>();
+	private final ArrayList<String> TOP_LINES = new ArrayList<>();
 
 	public ReadFile(String url) {
 		FILE = new File(url);
 		try {
 			Scanner scan = new Scanner(FILE);
 			while(scan.hasNextLine()) {
-				LINES.add(scan.nextLine());
+				String line = scan.nextLine();
+				if (line.contains(";")) {
+					LINES.add(line);
+				} else {
+					TOP_LINES.add(line);
+				}
 			}
 			scan.close();
 		} catch(Exception e) {
@@ -22,8 +28,20 @@ public class ReadFile {
 		}
 	}
 
-	public ArrayList<String> getLINES(){
-		return LINES;
+	public String[][] getLINES() {
+		String[][] lines = new String[LINES.size()][];
+		for (int i = 0; i < LINES.size(); i++) {
+			lines[i] = getLine(LINES.get(i));
+		}
+		return lines;
+	}
+
+	public ArrayList<String> getTOP_LINES() {
+		return TOP_LINES;
+	}
+
+	private String[] getLine(String line) {
+		return line.trim().split(";");
 	}
 
 }
