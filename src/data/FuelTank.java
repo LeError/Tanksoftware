0104190fa84947953d9b654b048e5d103cf55ff8
LeError <herder.robin@icloud.com>
2019-04-02 14:22:23 +0200

    Added Exceptions

--- src/data/FuelTank.java
+++ src/data/FuelTank.java
@@ -1,12 +1,18 @@
 package data;
 
+import exceptions.NumberOutOfRangeException;
+
 public class FuelTank {
 
 	private final int TANK_NUMBER;
 	private final float CAPACITY;
 	private float level;
 
-	public FuelTank(int tankNumber, float capacity, float level) {
+	public FuelTank(int tankNumber, float capacity, float level)
+	throws NumberOutOfRangeException {
+		if (level < 0 || level > 1) {
+			throw new NumberOutOfRangeException("Only values between 0 and 1 are allowed!");
+		}
 		TANK_NUMBER = tankNumber;
 		CAPACITY = capacity;
 		this.level = level;
