01f921797266137c421461eda0b4be7b7a20feb0
Robin Herder <herder.robin@icloud.com>
2019-04-12 19:55:05 +0200

    Menu navigation handling

--- src/gasStationSoftware/ui/WindowController.java
+++ src/gasStationSoftware/ui/WindowController.java
@@ -15,25 +15,36 @@ public class WindowController implements Initializable {
     private ImageView btnUser, btnSelling, btnInventory, btnTanks, btnEmployees, btnReports, btnSettings;
 
     @FXML
-    private AnchorPane userPane, sellingPane, inventoryPane, fuelTankPane, employeePane, reportPane;
+    private AnchorPane userPane, sellingPane, inventoryPane, fuelTankPane, employeePane, reportPane, settingsPane;
 
     @FXML
     private void handleMenuButtonAction(MouseEvent event) {
+        hidePanes();
         if (event.getTarget() == btnUser) {
-
+            userPane.setVisible(true);
         } else if (event.getTarget() == btnSelling) {
-
+            sellingPane.setVisible(true);
         } else if (event.getTarget() == btnInventory) {
-
+            inventoryPane.setVisible(true);
         } else if (event.getTarget() == btnTanks) {
-
+            fuelTankPane.setVisible(true);
         } else if (event.getTarget() == btnEmployees) {
-
+            employeePane.setVisible(true);
         } else if (event.getTarget() == btnReports) {
-
+            reportPane.setVisible(true);
         } else if (event.getTarget() == btnSettings) {
-
+            settingsPane.setVisible(true);
         }
+    }
+
+    private void hidePanes() {
+        userPane.setVisible(false);
+        sellingPane.setVisible(false);
+        inventoryPane.setVisible(false);
+        fuelTankPane.setVisible(false);
+        employeePane.setVisible(false);
+        reportPane.setVisible(false);
+        settingsPane.setVisible(false);
     }
 
     @Override
