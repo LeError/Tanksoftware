c9d4cb9f8e06db5dc60b889866f67b32e5f719e1
Robin Herder <herder.robin@icloud.com>
2019-04-26 17:35:37 +0200

    added fuel to tanks

--- src/main/java/gasStationSoftware/models/FuelTank.java
+++ src/main/java/gasStationSoftware/models/FuelTank.java
@@ -6,15 +6,17 @@ public class FuelTank {
 
     private final int TANK_NUMBER;
     private final float CAPACITY;
+    private Fuels fuel;
     private float level;
 
-    public FuelTank(int tankNumber, float capacity, float level)
+    public FuelTank(int tankNumber, float capacity, float level, Fuels fuel)
     throws NumberOutOfRangeException {
         if (level < 0 || level > 1) {
             throw new NumberOutOfRangeException("Only values between 0 and 1 are allowed!");
         }
         TANK_NUMBER = tankNumber;
         CAPACITY = capacity;
+        this.fuel = fuel;
         this.level = level;
     }
 
@@ -24,6 +26,14 @@ public class FuelTank {
 
     public float getCAPACITY() {
         return CAPACITY;
+    }
+
+    public Fuels getFuel() {
+        return fuel;
+    }
+
+    public void setFuel(Fuels fuel) {
+        this.fuel = fuel;
     }
 
     public float getLevel() {
