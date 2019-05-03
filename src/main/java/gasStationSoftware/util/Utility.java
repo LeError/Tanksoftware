011da6cefa10f282742cb68c5de9c741e87956e1
Robin Herder <herder.robin@icloud.com>
2019-05-03 21:43:39 +0200

    Moved util methods to utility class

--- src/main/java/gasStationSoftware/util/Utility.java
+++ src/main/java/gasStationSoftware/util/Utility.java
@@ -1,7 +1,21 @@
 package gasStationSoftware.util;
 
+import java.awt.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Utility {
 
+    public static Color hex2Rgb(String colorStr) {
+        return new Color(
+                Integer.valueOf( colorStr.substring( 1, 3 ), 16 ),
+                Integer.valueOf( colorStr.substring( 3, 5 ), 16 ),
+                Integer.valueOf( colorStr.substring( 5, 7 ), 16 )
+        );
+    }
 
+    public static String getDate() {
+        return new SimpleDateFormat("dd.MM.yyyy").format(new Date());
+    }
 
 }
