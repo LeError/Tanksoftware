4659505fb3c87834b1316a2df0a2d23094a329e5
Robin Herder <herder.robin@icloud.com>
2019-05-04 14:27:50 +0200

    amount

--- src/main/java/gasStationSoftware/models/Fuel.java
+++ src/main/java/gasStationSoftware/models/Fuel.java
@@ -2,9 +2,22 @@ package gasStationSoftware.models;
 
 public class Fuel extends Item {
 
-    private float amount = 0;
+    private float amount;
 
-    public Fuel(int inventoryNumber, String label, float price, String currency) {
+    public Fuel(int inventoryNumber, String label, float price, String currency, float amount) {
         super(inventoryNumber, label, price, currency);
+        this.amount = amount;
+    }
+
+    public float getAmount(){
+        return amount;
+    }
+
+    public void addAmount(float amount){
+        this.amount += amount;
+    }
+
+    public void removeAmount(float amount){
+        this.amount -= amount;
     }
 }
