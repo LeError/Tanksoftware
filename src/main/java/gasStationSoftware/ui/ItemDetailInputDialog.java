087e6c47fefcda051833d24ee833b30626b1bfbb
Robin Herder <herder.robin@icloud.com>
2019-05-12 18:39:33 +0200

    changed processing of Input

--- src/main/java/gasStationSoftware/ui/ItemDetailInputDialog.java
+++ src/main/java/gasStationSoftware/ui/ItemDetailInputDialog.java
@@ -1,7 +1,7 @@
 package gasStationSoftware.ui;
 
+import com.jfoenix.controls.JFXTextField;
 import gasStationSoftware.controller.WindowController;
-import gasStationSoftware.models.InventoryType;
 import gasStationSoftware.models.ItemType;
 import gasStationSoftware.util.Dialog;
 import javafx.scene.layout.AnchorPane;
@@ -9,15 +9,27 @@ import javafx.scene.layout.StackPane;
 
 public class ItemDetailInputDialog extends Dialog {
 
-    public ItemDetailInputDialog(StackPane rootPane, WindowController windowController, InventoryType type, ItemType iType) {
-        super(windowController);
+    private ItemType iType;
 
-        AnchorPane pane = getAnchorPane(300, 200);
-        if(type == InventoryType.Fuel) {
-            inputDialog(rootPane, pane, "Kraftstoff " + iType.getLABEL() + " einbuchen");
-        } else if(type == InventoryType.Fuel) {
-            inputDialog(rootPane, pane, "Produkt " + iType.getLABEL() + " einbuchen");
-        }
+    public ItemDetailInputDialog(StackPane rootPane, WindowController windowController, ItemType iType) {
+        super(windowController);
+        this.iType = iType;
+
+        JFXTextField txtItem = getTextfield(200, 30, true, 10d, 5d, 5d);
+        txtItem.setText(iType.getLABEL());
+
+        JFXTextField txtAmount = getTextfield(200, 30, false, 60d, 5d, 5d);
+        txtAmount.setPromptText("Anzahl in l");
+
+        JFXTextField txtPrice = getTextfield(200, 30, false, 110d, 5d, 5d);
+        txtPrice.setPromptText("Preis");
+
+        JFXTextField txtCurrency = getTextfield(200, 30, true, 160d, 5d, 5d);
+        txtCurrency.setText("EUR");
+
+        AnchorPane pane = getAnchorPane(210, 200);
+        pane.getChildren().addAll(txtItem, txtAmount, txtPrice, txtCurrency);
+        inputDialog(rootPane, pane, "Kraftstoff " + iType.getLABEL() + " einbuchen");
     }
 
     @Override
