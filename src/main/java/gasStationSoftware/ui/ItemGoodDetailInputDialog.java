e0ff81d8025cbd03858e379ff0c57ef26f4a7adb
d073980 <robin.herder@sap.com>
2019-05-17 11:27:56 +0200

    added base fields

--- src/main/java/gasStationSoftware/ui/ItemGoodDetailInputDialog.java
+++ src/main/java/gasStationSoftware/ui/ItemGoodDetailInputDialog.java
@@ -1,5 +1,6 @@
 package gasStationSoftware.ui;
 
+import com.jfoenix.controls.JFXTextField;
 import gasStationSoftware.controller.WindowController;
 import gasStationSoftware.models.ItemType;
 import gasStationSoftware.util.Dialog;
@@ -15,7 +16,20 @@ extends Dialog {
         super(windowController);
         this.iType = iType;
 
+        JFXTextField txtItem = getTextfield(200, 30, true, 10d, 5d, 5d);
+        txtItem.setText(iType.getLABEL());
+
+        JFXTextField txtAmount = getTextfield(200, 30, true, 60d, 5d, 5d);
+        txtAmount.setText("0");
+
+        JFXTextField txtPrice = getTextfield(200, 30, false, 110d, 5d, 5d);
+        txtPrice.setPromptText("Preis");
+
+        JFXTextField txtCurrency = getTextfield(200, 30, true, 160d, 5d, 5d);
+        txtCurrency.setText("EUR");
+
         AnchorPane pane = getAnchorPane(300, 200);
+        pane.getChildren().addAll(txtItem, txtAmount, txtPrice, txtCurrency);
         inputDialog(rootPane, pane, "Produkt " + iType.getLABEL() + " einbuchen");
     }
 
