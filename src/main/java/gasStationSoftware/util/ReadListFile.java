468ca580f7c43b667f46729f01721855b41e057f
Robin Herder <herder.robin@icloud.com>
2019-05-26 13:35:04 +0200

    added methods

--- src/main/java/gasStationSoftware/util/ReadListFile.java
+++ src/main/java/gasStationSoftware/util/ReadListFile.java
@@ -1,9 +1,42 @@
 package gasStationSoftware.util;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+
 public class ReadListFile {
 
-    public ReadListFile(String url) {
+    private final File FILE;
+    private String date;
+    private final ArrayList<String> LINES = new ArrayList<>();
 
+    public ReadListFile(String url) {
+        FILE = new File(url);
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(FILE))) {
+            String line;
+            boolean firstLine = true;
+            date = bufferedReader.readLine();
+            while((line = bufferedReader.readLine()) != null) {
+                LINES.add(line);
+            }
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
     }
+
+    public String[][] getLINES() {
+        String[][] lines = new String[LINES.size()][2];
+        for(int i = 0; i < LINES.size(); i++) {
+            lines[i] = getLine(LINES.get(i));
+        }
+        return lines;
+    }
+
+    private String[] getLine(String line) {
+        return line.trim().split("=");
+    }
+
+
 
 }
