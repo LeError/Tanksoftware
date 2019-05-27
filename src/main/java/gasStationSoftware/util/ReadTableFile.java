d98f34d177612a7323ede78200512360aefd1098
d073980 <robin.herder@sap.com>
2019-05-27 10:53:15 +0200

    getDate deleted unused

--- src/main/java/gasStationSoftware/util/ReadTableFile.java
+++ src/main/java/gasStationSoftware/util/ReadTableFile.java
@@ -4,7 +4,10 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class ReadTableFile {
 
@@ -69,15 +72,6 @@ public class ReadTableFile {
 
     /**
      *
-     * @return boolean
-     * @author Robin Herder
-     */
-    private boolean isWindows() {
-        return System.getProperty("os.name").toLowerCase().contains("win");
-    }
-
-    /**
-     *
      * @param path
      * @return boolean
      * @author Robin Herder
@@ -92,6 +86,15 @@ public class ReadTableFile {
             return true;
         }
         return false;
+    }
+
+    public Date getDate() {
+        try {
+            return new SimpleDateFormat("dd.MM.yyyy").parse(getLine(TOP_LINES.get(0))[1]);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
 }
