6bd4e3d767732b8c1aa695ef5ac1a8b42bf6a7de
d073980 <robin.herder@sap.com>
2019-06-03 16:11:11 +0200

    minor changes

--- src/main/java/gasStationSoftware/models/CustomerOrder.java
+++ src/main/java/gasStationSoftware/models/CustomerOrder.java
@@ -13,11 +13,13 @@ extends Document {
     private HashMap<Fuel, Float> fuels = new HashMap<>();
     private HashMap<Good, Integer> goods = new HashMap<>();
 
+    private final int RECEIPT_NUMBER;
     private final Employee EMPLOYEE;
 
     public CustomerOrder(int receiptNumber, Date date, Employee employee, ArrayList<Fuel> fuels,
     ArrayList<Good> goods) {
-        super(DocumentType.receipt, String.valueOf(receiptNumber), date);
+        super(DocumentType.receipt, "RECEIPT_" + receiptNumber, date);
+        RECEIPT_NUMBER = receiptNumber;
         EMPLOYEE = employee;
         for(Fuel fuel : fuels) {
             this.fuels.put(fuel, fuel.getCheckoutAmount());
@@ -43,7 +45,7 @@ extends Document {
         }
         while(goodKey.hasNext()) {
             Good good = goodKey.next();
-            total +=  good.getPrice() * fuels.get(good);
+            total += good.getPrice() * goods.get(good);
         }
         return Utility.round(total, 2);
     }
@@ -89,7 +91,7 @@ extends Document {
     }
 
     public int getRECEIPT_NUMBER() {
-        return Integer.parseInt(getNAME());
+        return RECEIPT_NUMBER;
     }
 
     public Employee getEMPLOYEE() {
