1c59f5618cd669789a6d0ce8be0715e7380c88cf
LeError <herder.robin@Icloud.com>
2019-06-06 22:54:40 +0200

    textfield for orders

---
+++ src/main/java/gasStationSoftware/util/TextFielCustom.java
@@ -0,0 +1,31 @@
+package gasStationSoftware.util;
+
+import com.jfoenix.controls.JFXTextField;
+import gasStationSoftware.models.Item;
+import javafx.scene.control.TableCell;
+
+public class TextFielCustom extends TableCell<Item, Double> {
+    private JFXTextField amount = new JFXTextField();
+
+    /**
+     * Setzt grafik und wert
+     * @author Robin Herder
+     */
+    @Override
+    protected void updateItem(Double item, boolean empty) {
+        super.updateItem(item, empty);
+        if (!empty) {
+            setGraphic(amount);
+            setAmount(item);
+        }
+    }
+
+    /**
+     * setzt progress und styling
+     * @author Robin Herder
+     */
+    public void setAmount(Double value) {
+        amount.setText(String.valueOf(value));
+        amount.setMinSize(100, 30);
+    }
+}
