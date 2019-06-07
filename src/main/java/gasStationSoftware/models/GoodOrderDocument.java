2ab8342c806ccd39e69cc28546b3ebb6cb6afb81
d073980 <robin.herder@sap.com>
2019-06-07 14:23:26 +0200

    changed type

--- src/main/java/gasStationSoftware/models/GoodOrderDocument.java
+++ src/main/java/gasStationSoftware/models/GoodOrderDocument.java
@@ -6,8 +6,7 @@ import java.util.Date;
 public class GoodOrderDocument
 extends Document {
 
-    private final ArrayList<Good> GOODS;
-    private final ArrayList<Integer> GOODS_AMOUNT;
+    private final ArrayList<Item> GOODS;
 
     /**
      * Constructor GoodOrderDocument
@@ -16,14 +15,11 @@ extends Document {
      * @param name        name des dokuments
      * @param date        erstellungsdatum
      * @param goods       wahren im doc
-     * @param goodsAmount wahrenanz im doc
      * @author Robin Herder
      */
-    public GoodOrderDocument(DocumentType docType, String name, Date date, ArrayList<Good> goods,
-    ArrayList<Integer> goodsAmount) {
+    public GoodOrderDocument(DocumentType docType, String name, Date date, ArrayList<Item> goods) {
         super(docType, name, date);
         GOODS = goods;
-        GOODS_AMOUNT = goodsAmount;
     }
 
     /**
@@ -37,7 +33,7 @@ extends Document {
         lines.add("Bestelldatum=" + getDATE());
         lines.add("Warennummer;Bestellmenge");
         for (int i = 0; i < GOODS.size(); i++) {
-            lines.add(GOODS.get(i).getINVENTORY_NUMBER() + ";" + GOODS_AMOUNT.get(i));
+            lines.add(GOODS.get(i).getINVENTORY_NUMBER() + ";" + GOODS.get(i).getCheckoutAmount());
         }
         return lines;
     }
