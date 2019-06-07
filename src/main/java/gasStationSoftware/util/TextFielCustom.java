94e5f19eb44b8e4f6abd59980102a9a90924418c
d073980 <robin.herder@sap.com>
2019-06-07 14:25:27 +0200

    addListener

--- src/main/java/gasStationSoftware/util/TextFielCustom.java
+++ src/main/java/gasStationSoftware/util/TextFielCustom.java
@@ -4,19 +4,25 @@ import com.jfoenix.controls.JFXTextField;
 import gasStationSoftware.models.Item;
 import javafx.scene.control.TableCell;
 
-public class TextFielCustom extends TableCell<Item, Double> {
+public class TextFielCustom
+extends TableCell<Item, Float> {
     private JFXTextField amount = new JFXTextField();
+
+    private float item = -1;
 
     /**
      * Setzt grafik und wert
      * @author Robin Herder
      */
-    @Override
-    protected void updateItem(Double item, boolean empty) {
+    @Override protected void updateItem(Float item, boolean empty) {
         super.updateItem(item, empty);
         if (!empty) {
             setGraphic(amount);
-            setAmount(item);
+            if (this.item == -1) {
+                setAmount(item);
+            } else {
+                setAmount(this.item);
+            }
         }
     }
 
@@ -24,10 +30,18 @@ public class TextFielCustom extends TableCell<Item, Double> {
      * setzt größe und wert
      * @author Robin Herder
      */
-    public void setAmount(Double value) {
+    public void setAmount(Float value) {
         amount.setText(String.valueOf(value));
         amount.setMinSize(50, 30);
         amount.setPrefSize(50, 30);
         amount.setMaxSize(50, 30);
+        amount.textProperty().addListener((observable, oldValue, newValue) -> {
+            try {
+                super.updateItem(Float.parseFloat(newValue), true);
+                this.item = Float.parseFloat(newValue);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
     }
 }
